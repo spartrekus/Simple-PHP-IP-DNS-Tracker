a0e22c48e41a61b9077ec99f870740aca5a23f81
spartrekus <spartrekus@gmail.com>
2017-08-16 10:39:03 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<h1> IP CHECKER </h1>
+
+<?php
+
+$externalContent = file_get_contents('http://checkip.dyndns.com/');
+preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
+$externalIp = $m[1];
+
+$mytime = date("h:i:sa");
+$mydate = date("Y/m/d");
+
+$ip=$_SERVER['REMOTE_ADDR'];
+
+$file = fopen("123973972733.ip","a");
+ fwrite($file, "TIME/DATE: ");
+ fwrite($file, $mydate );
+ fwrite($file, " "  );
+ fwrite($file, $mytime );
+ fwrite($file, ", EXTIP: " );
+ fwrite($file, $externalIp );
+ fwrite($file, ", IP: " );
+ fwrite($file,$ip);
+ fwrite($file, "\n" );
+fclose($file);
+?> 
+
+
